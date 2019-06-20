e737cbc4e17a468162663d1d0f92797c20c768da
Phat Hoang <phathk.hn@gmail.com>
2019-06-20 18:31:40 +0800

    User - Nomarlize and validate email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,3 +12,17 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(user.email, email)
+
+    def test_create_user_email_nomarlized(self):
+        email = 'haha@GMAIL.com'
+        password = '123123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_create_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '1234')
